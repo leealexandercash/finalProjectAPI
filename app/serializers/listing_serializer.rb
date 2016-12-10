a3e4d80177bb370fc8f9ee90c560e028540c6bf9
Lee Cash <leealexandercash@gmail.com>
2016-12-10 18:39:30 +0000

    image uploader set up minus keys

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,9 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :image, :name, :price, :description
+  attributes :id, :image, :name, :price, :description, :image
   has_one :user
   has_one :place
+
+  def image
+    object.image.url
+  end
 end
